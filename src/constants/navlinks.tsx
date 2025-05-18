0d7cf853e6e3961ff62bece9545ca09babbb4a0f
Rubytech1 <suheybh06@gmail.com>
2025-05-18 16:32:27 -0400

    Remove articles, about section and resume button

--- src/constants/navlinks.tsx
+++ src/constants/navlinks.tsx
@@ -1,9 +1,7 @@
 import {
-  IconArticle,
   IconBolt,
   IconBriefcase2,
   IconMail,
-  IconMessage2,
 } from "@tabler/icons-react";
 
 export const navlinks = [
@@ -13,19 +11,9 @@ export const navlinks = [
     icon: IconBolt,
   },
   {
-    href: "/about",
-    label: "About",
-    icon: IconMessage2,
-  },
-  {
     href: "/projects",
     label: "Projects",
     icon: IconBriefcase2,
-  },
-  {
-    href: "/blog",
-    label: "Articles",
-    icon: IconArticle,
   },
   {
     href: "/contact",
