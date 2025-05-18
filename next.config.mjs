e084b17632fa09228ca1cf9c558b7a648a524b93
Rubytech1 <suheybh06@gmail.com>
2025-05-18 17:10:50 -0400

    Add imgur.com to allowed image domains

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,7 @@ const nextConfig = {
     domains: [
       "images.unsplash.com",
       "res.cloudinary.com",
-      "images.pexels.com",
-      "imgur.com"
+      "images.pexels.com"
     ],
   },
   experimental: {
