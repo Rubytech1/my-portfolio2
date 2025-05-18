acc6fb0f4272f3ec3553b7b14be0ef836d4d5dab
Rubytech1 <suheybh06@gmail.com>
2025-05-18 17:10:51 -0400

    Add imgur.com to allowed image domains

---
+++ src/constants/products.tsx
@@ -0,0 +1,136 @@
+import { Product } from "@/types/products";
+
+export const products: Product[] = [
+  {
+    href: "http://torontoctagon.com/",
+    title: "Torontoctagon",
+    description:
+      "A creative MMA agency based in Toronto, building sleek signup forms for seamless digital experiences.",
+    thumbnail: "https://imgur.com/a/VxX0Cdh",
+    images: [
+      "https://imgur.com/a/VxX0Cdh",
+      "https://imgur.com/a/VxX0Cdh"
+    ],
+    stack: ["Nextjs", "Tailwindcss"],
+    slug: "torontoctagon",
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "https://algochurn.com",
+    title: "Algochurn",
+    description:
+      "Practice for technical interviews with hands on coding challenges.",
+    thumbnail: "https://images.pexels.com/photos/18023772/pexels-photo-18023772.jpeg",
+    images: [
+      "https://images.pexels.com/photos/18023772/pexels-photo-18023772.jpeg",
+      "https://images.pexels.com/photos/18023772/pexels-photo-18023772.jpeg"
+    ],
+    stack: ["Nextjs", "Tailwindcss"],
+    slug: "algochurn",
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "https://gomoonbeam.com",
+    title: "Moonbeam",
+    description:
+      "Never write from scratch again with Moonbeam, your AI first writing tool",
+    thumbnail: "https://images.pexels.com/photos/18071319/pexels-photo-18071319.jpeg",
+    images: [
+      "https://images.pexels.com/photos/18071319/pexels-photo-18071319.jpeg",
+      "https://images.pexels.com/photos/18071319/pexels-photo-18071319.jpeg"
+    ],
+    stack: ["Nextjs", "Tailwindcss"],
+    slug: "moonbeam",
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "https://tailwindmasterkit.com",
+    title: "Tailwind Master Kit",
+    description:
+      "A beautiful and comprehensive Tailwind CSS components library for building modern websites and applications.",
+    thumbnail: "https://images.pexels.com/photos/18073372/pexels-photo-18073372.jpeg",
+    images: [
+      "https://images.pexels.com/photos/18073372/pexels-photo-18073372.jpeg",
+      "https://images.pexels.com/photos/18073372/pexels-photo-18073372.jpeg"
+    ],
+    stack: ["Nextjs", "Tailwindcss"],
+    slug: "tailwindmasterkit",
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
+];
