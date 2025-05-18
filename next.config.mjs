2d9f521b9913f897cfea5ac146d5dcc39f8f35e7
Rubytech1 <suheybh06@gmail.com>
2025-05-18 17:10:31 -0400

    Update Next.js configuration to allow Imgur images

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,33 @@ import rehypePrism from "@mapbox/rehype-prism";
 
 const nextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",
-      "res.cloudinary.com",
-      "images.pexels.com",
-      "imgur.com"
-    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        pathname: '/**',
+      }
+    ]
   },
   experimental: {
     mdxRs: true,
