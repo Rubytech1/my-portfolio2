3cb7d2346c5e0d3a565d619b9c690e0123f126da
Rubytech1 <suheybh06@gmail.com>
2025-05-18 21:21:39 -0400

    Fix missing Next.js BUILD_ID file

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,9 @@ const nextConfig = {
       "images.pexels.com"
     ],
   },
+  experimental: {
+    mdxRs: true,
+  },
 };
 
 const withMDX = nextMDX({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <span className="text-4xl">👋</span>
       <Heading className="font-black">Hello there! I&apos;m Suheyb</Heading>
       <Paragraph className="max-w-xl mt-4">
-       I&apos;m a front-end developer based in Toronto, Canada. I have worked on a wide range of front-end projects, from Fitness applications to Transportation booking platforms, focusing on creating clean, well-crafted interfaces that look great and provide a seamless user experience.
+       I'm a front-end developer based in Toronto, Canada. I have worked on a wide range of front-end projects, from Fitness applications to Transportation booking platforms, focusing on creating clean, well-crafted interfaces that look great and provide a seamless user experience.
       </Paragraph>
       <Heading
         as="h2"
