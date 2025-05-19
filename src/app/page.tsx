7af7fefd5ba8b531f3ed1b0182a478a065c2bf7d
Rubytech1 <suheybh06@gmail.com>
2025-05-18 22:06:33 -0400

    Remove tech stack and projects sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import { Container } from "@/components/Container";
 import { Heading } from "@/components/Heading";
-import { Highlight } from "@/components/Highlight";
 import { Paragraph } from "@/components/Paragraph";
-import { Products } from "@/components/Products";
-import { TechStack } from "@/components/TechStack";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,16 +8,8 @@ export default function Home() {
       <span className="text-4xl">👋</span>
       <Heading className="font-black">Hello there! I&apos;m Suheyb</Heading>
       <Paragraph className="max-w-xl mt-4">
-       I'm a front-end developer based in Toronto, Canada. I have worked on a wide range of front-end projects, from Fitness applications to Transportation booking platforms, focusing on creating clean, well-crafted interfaces that look great and provide a seamless user experience.
+        I&apos;m a front-end developer based in Toronto, Canada. I have worked on a wide range of front-end projects, from Fitness applications to Transportation booking platforms, focusing on creating clean, well-crafted interfaces that look great and provide a seamless user experience.
       </Paragraph>
-      <Heading
-        as="h2"
-        className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
-      >
-        What I&apos;ve been working on
-      </Heading>
-      <Products />
-      <TechStack />
     </Container>
   );
 }
--- src/constants/navlinks.tsx
+++ src/constants/navlinks.tsx
@@ -1,6 +1,5 @@
 import {
   IconBolt,
-  IconBriefcase2,
   IconMail,
 } from "@tabler/icons-react";
 
@@ -9,11 +8,6 @@ export const navlinks = [
     href: "/",
     label: "Home",
     icon: IconBolt,
-  },
-  {
-    href: "/projects",
-    label: "Projects",
-    icon: IconBriefcase2,
   },
   {
     href: "/contact",
